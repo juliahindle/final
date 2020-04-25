a28cb761d66cbad01cd6e5f7c539538806bc8272
Ankur Dahal <48399122+dahalankur@users.noreply.github.com>
2020-04-25 14:33:54 -0500

    Update match.js

--- match.js
+++ match.js
@@ -18,13 +18,8 @@ async function main()
 		res.writeHead(200, {'Content-Type':'text/html'});
 		var qobj = url.parse(req.url, true).query;
 		var zip = qobj.zip;
-		find_donor(zip, res);
-	}).listen(port);
-}
-main().catch(console.error);
 
-async function find_donor(zip, res){
-	if (zip != null)
+		if (zip != null)
 	{
 		MongoClient.connect(uri, { useUnifiedTopology: true }, async function(err, client) {
 			if(err) { return console.log(err); }
@@ -48,14 +43,49 @@ async function find_donor(zip, res){
 					}
 				} 
 				console.log("MIN DIST: " + min_dist)
-	            res.end();
 	        });
 		});
 	}
+	res.end();
+	}).listen(port);
 }
+main().catch(console.error);
 
 
-async function get_dist(zip, comp_zip){
+
+// async function find_donor(zip, res){
+// 	if (zip != null)
+// 	{
+// 		MongoClient.connect(uri, { useUnifiedTopology: true }, async function(err, client) {
+// 			if(err) { return console.log(err); }
+
+// 			let dbo = client.db("masks");
+// 			let collection = dbo.collection('mask_donors');
+
+// 			await collection.find().toArray(function(err, result) {
+// 	            if (err) { return console.log(err); }
+// 	            var query = result;
+// 	            console.log(query);
+// 	            client.close();
+// 	            var min_dist = zipcodes.distance(zip, query[0].zip);
+// 	            var curr_dist = 0;
+// 	            for (index = 0; index < query.length; index++) { 
+// 	            	curr_dist = zipcodes.distance(zip, query[index].zip);
+// 	            	console.log ("CURR DIST BETWEEN: " + zip + " and " + query[index].zip + " is " + curr_dist);
+// 					if (curr_dist < min_dist && curr_dist != null)
+// 					{
+// 						min_dist = curr_dist;
+// 					}
+// 				} 
+// 				console.log("MIN DIST: " + min_dist)
+// 	            res.end();
+// 	        });
+// 		});
+// 	}
+// }
+
+
+//async function get_dist(zip, comp_zip){
 		
 
 	/*
@@ -92,4 +122,4 @@ async function get_dist(zip, comp_zip){
 	    }); 
 	}); 
 	*/
-}
+//}
