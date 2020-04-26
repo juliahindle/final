ca7d4938bce29abf6162d280ebc58a94559e95fb
amanci0 <60190150+amanci0@users.noreply.github.com>
2020-04-25 19:56:07 -0500

    Create indexOf.js

--- donations.js
+++ donations.js
@@ -5,10 +5,12 @@ const MongoClient = require('mongodb').MongoClient;
 var http = require('http');
 var url = require('url');
 var port = process.env.PORT || 8080; // to enable access by heroku
+const fs = require('fs');
 
 async function main()
 {
-	http.createServer(function(req, res){
+
+	async function insertData (req, res){
 		res.writeHead(200, {'Content-Type':'text/html'});
 		var qobj = url.parse(req.url, true).query;
 		var f_name = qobj.f_name;
@@ -16,32 +18,43 @@ async function main()
 		var mask_num = qobj.masks;
 		var zip = qobj.zip;
 		var state = qobj.state;
-		add_to_db(f_name, l_name, mask_num, zip, state);
+		var phone = qobj.phone;
 
-	    res.end();
-	}).listen(port);
+		if (f_name != null){
+			const uri = "mongodb+srv://webalub:comp20tufts@cluster0-jnsgs.mongodb.net/test?retryWrites=true&w=majority"
+
+			await MongoClient.connect(uri, async function(err, client) {
+				if(err) { return console.log(err); }	  
+				var dbo = client.db("masks");
+				var collection = dbo.collection('mask_donors');
+
+				var newData = {"f_name": f_name, "l_name": l_name, "mask_num": mask_num, 
+							   "zip": zip, "state": state, "phone": phone};
+				await collection.insertOne(newData, function(err, res) {
+				if (err) throw err;
+				console.log("new document inserted");
+				}   );	
+
+				console.log("Success!");
+				client.close();
+			});
+	}
+	await fs.readFile('./thanks.html', null, function (err, data) {
+		if (err) {
+			res.writeHead(404);
+			res.write('File Not Found');
+		} else {
+			res.write(data);
+		}
+		res.end();
+	}	
+	); 
+	
+	}
+
+	http.createServer(insertData).listen(port);
+
 }
 main().catch(console.error);
 
-async function add_to_db(f_name,l_name, mask_num, zip, state){
-	if (f_name != null){
-		const uri = "mongodb+srv://webalub:comp20tufts@cluster0-jnsgs.mongodb.net/test?retryWrites=true&w=majority"
-
-		MongoClient.connect(uri, function(err, client) {
-			if(err) { return console.log(err); }	  
-			var dbo = client.db("masks");
-			var collection = dbo.collection('mask_donors');
-
-			var newData = {"f_name": f_name, "l_name": l_name, "mask_num": mask_num, 
-						   "zip": zip, "state": state};
-			collection.insertOne(newData, function(err, res) {
-			if (err) throw err;
-			console.log("new document inserted");
-			}   );	
-
-			console.log("Success!");
-			client.close();
-		});
-	}
-}
 
---
+++ match.js
@@ -0,0 +1,106 @@
+//Install zip codes module with npm i zipcodes
+// USED https://github.com/davglass/zipcodes as a zip code module
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://webalub:comp20tufts@cluster0-jnsgs.mongodb.net/test?retryWrites=true&w=majority";
+var port = process.env.PORT || 8080; // to enable access by heroku
+
+var http = require('http');
+var url = require('url');
+var zipcodes = require('zipcodes');
+var my_data = [];
+var donation_index = -1;
+
+
+async function main()
+{
+	console.log("MAIN");
+	http.createServer(async function(req, res){
+		res.writeHead(200, {'Content-Type':'text/html'});
+		var qobj = url.parse(req.url, true).query;
+		var zip = qobj.zip;
+
+	// ---------------------- start of if-block -----------------------
+	if (zip != null)
+	{
+		MongoClient.connect(uri, { useUnifiedTopology: true }, async function(err, client) {
+			if(err) { return console.log(err); }
+
+			let dbo = client.db("masks");
+			let collection = dbo.collection('mask_donors');
+
+			await collection.find().toArray(function(err, result) {
+	            if (err) { return console.log(err); }
+	            var query = result;
+	            my_data = query;
+	            console.log(query);
+	            client.close();
+	            var min_dist = zipcodes.distance(zip, query[0].zip);
+	            var curr_dist = 0;
+	            for (index = 0; index < query.length; index++) { 
+	            	curr_dist = zipcodes.distance(zip, query[index].zip);
+	            	console.log ("CURR DIST BETWEEN: " + zip + " and " + query[index].zip + " is " + curr_dist);
+					if (curr_dist < min_dist && curr_dist != null)
+					{
+						min_dist = curr_dist;
+						donation_index = index;
+					}
+				} 
+				try {
+					console.log("MIN DIST: " + min_dist)
+					console.log("Donator name: " + query[donation_index].f_name + " " + query[donation_index].l_name);
+					console.log("Masks available: " + query[donation_index].mask_num);
+					console.log("State: " + query[donation_index].state);
+					console.log("Phone number: " + query[donation_index].phone);
+				} catch(e) {
+					console.log(e);
+				}
+	        });
+		});
+	}
+	// ---------------------- end of if-block -----------------------
+
+	await sleep(1000);
+
+	// ---------------------- write to the page ----------------------
+	try{
+		var zipcode = my_data[donation_index].zip;
+		var lat = zipcodes.lookup(zipcode).latitude;
+		var lng = zipcodes.lookup(zipcode).longitude;
+		/* map api headers */
+		res.write('<html><head><title>Find Masks Near You</title></head><link rel="stylesheet" type="text/css" href="https://juliahindle.github.io/final/style.css" />');
+		res.write('<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" /><script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script><link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="crossorigin=""/><script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="crossorigin=""></script>');
+		/* ------------------ */
+
+
+
+		res.write("<body><h1>Find Masks Near Your Location</h1>");
+		res.write("<div class='result'> The person closest to your location is " + my_data[donation_index].f_name + " " + my_data[donation_index].l_name + ".");
+		res.write("<br /> They have " + my_data[donation_index].mask_num + " masks available. You can reach them at their phone number, " + my_data[donation_index].phone + ".</div>");
+		res.write("You can view the approximate location of the donator below: <br /> <div class='map' id='map' style='height:450px'></div>");
+		var attribution = "{ attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors' }";
+		res.write("<script type='text/javascript'>var map = L.map('map').setView(["+lat+", "+lng+"], 13);L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', "+ attribution+").addTo(map);L.marker(["+lat+", "+lng+"]).addTo(map)</script>");
+	} catch(e) {
+		res.write("<script type='text/javascript'>alert('The zip code you entered may not be a valid zip code. Please try again.')</script>");
+		console.log(e);
+	}
+	finally{
+	res.write("<p>To get back, please click <a href='https://juliahindle.github.io/final/donations.html'>here</a></p>");
+    res.end("<footer><div>&copy; Team Webalubadubdub, 2020</div></footer></body></html>");
+}
+// ------------------------- end of write ------------------------------
+
+}).listen(port);
+}
+main().catch(console.error);
+
+
+
+// function sleep - waits for the specified amount of time (in milliseconds)
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}   
+
